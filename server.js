09ae5e5dfffb5583fcce698ba8d27c7cca697745
scottbisese <bisese.scott@gmail.com>
2020-04-21 11:02:55 -0500

    added homepage function and some good ole server.js stuff

--- server.js
+++ server.js
@@ -8,16 +8,28 @@ const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
 
 // Application Setup
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors()); //middleware
+app.use(express.static('public'));
+
+//view engine/templating
+app.set('view engine', 'ejs');
 
 //routes
 app.get('/', (request, response) => {
     response.send('Home Page!');
 });
+
+function showHomepage(req,res) {
+    client.query('SELECT * FROM stories;').then(stories => {
+      res.render('pages/homepage',{stories:stories.rows});
+    }).catch(getErrorHandler(res));
+  }
 
 
 
