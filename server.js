b0be4f716a3705aec5ef40b8cb675665f683d07c
bproorda <bproorda@gmail.com>
2020-04-20 21:13:10 -0500

    added backup port number for heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const superagent = require('superagent');
 const pg = require('pg');
 
 // Application Setup
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors()); //middleware
 
