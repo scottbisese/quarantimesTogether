b867a2de3bcd69819da6b250176328b7c6a4a164
bproorda <bproorda@gmail.com>
2020-04-23 14:08:37 -0500

    switched stories sql to descending order by id

--- modules/stories.js
+++ modules/stories.js
@@ -5,7 +5,7 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 function showStories(req,res) {
-    client.query('SELECT * FROM stories;').then(stories => {
+    client.query('SELECT * FROM stories ORDER BY id DESC;').then(stories => {
       res.render('pages/stories',{stories:stories.rows});
     }).catch(err =>
         handleError(err, req, res));
