004f824a01a968974ec093b66169f10698631f1e
bproorda <bproorda@gmail.com>
2020-04-23 14:31:19 -0500

    added button/fuction to clear filters on stories page

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ console.log('chart.js is running');
 
   //event listener
   $('select').change(hideElement);
-  let $section = $('<section>').attr('data-keyword', story.category);
+//   let $section = $('<section>').attr('data-keyword', story.category);
   
   //hidey filter gafiltafish
    function hideElement() { 
@@ -14,3 +14,7 @@ console.log('chart.js is running');
      $(`section.${value}`).slideDown(888); 
      }                                       
   }
+  $('#clearFilter').on('click', ()=>{
+      console.log('filters cleared');
+    $('section').slideDown(888); 
+  })
